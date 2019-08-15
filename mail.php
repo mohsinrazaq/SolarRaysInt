a8ab90b448f2e70f548fff6cc0a22c8d59746e04
Mohsin Razzaq <33516879+mohsinrazaq@users.noreply.github.com>
2019-08-15 20:44:29 +0500

    cleaned mail.php code

--- mail.php
+++ mail.php
@@ -5,12 +5,12 @@
     $number = $_POST['user_number'];
     $message = $_POST['user_message'];
  $mailto = " email adress where you want to send email"
-    e.g "mohsinrazzaq25@gmail.com";
+            e.g "mohsinrazzaq25@gmail.com";
  $subject = "Response from website or can be any";
- $headers = " from :" $email; 
- $txt = "you have received an email from  .$username".
-    "\n\n"."Email of the sender is" .$email. "\n\n". 
- "Contact of sender is :" .$number ."\n\n". "message is :".$message; 
+ $headers = " from :" .$email; 
+ $txt = "you have received an email from"  .$username 
+    ."\n\n" ."Email of the sender is" .$email ."\n\n"
+    ."Contact of sender is :" .$number ."\n\n" ."message is :" .$message; 
  $email($mailto, $subject, $txt, $headers); 
  header("location:index.html"); 
  }
