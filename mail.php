b54ff0de7facc2f4378e74d044bf1068e703c308
Mohsin Razzaq <33516879+mohsinrazaq@users.noreply.github.com>
2019-08-15 22:45:40 +0500

    completed mail.php file

--- mail.php
+++ mail.php
@@ -8,9 +8,10 @@
             e.g "mohsinrazzaq25@gmail.com";
  $subject = "Response from website or can be any";
  $headers = " from :" .$email; 
- $txt = "you have received an email from"  .$username 
-    ."\n\n" ."Email of the sender is" .$email ."\n\n"
-    ."Contact of sender is :" .$number ."\n\n" ."message is :" .$message; 
+ $txt = "you have received an email from"  .$username ."\n\n" 
+        ."Email of the sender is" .$email ."\n\n"
+        ."Contact of sender is :" .$number ."\n\n" 
+        ."message is :" .$message; 
  $email($mailto, $subject, $txt, $headers); 
  header("location:index.html"); 
  }
