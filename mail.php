9d3e341cdee9ea61d4ca52894f2708e62a273c4a
Mohsin Razzaq <33516879+mohsinrazaq@users.noreply.github.com>
2019-08-15 20:41:31 +0500

    added mail.php script

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+     if(isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $number = $_POST['user_number'];
+    $message = $_POST['user_message'];
+ $mailto = " email adress where you want to send email"
+    e.g "mohsinrazzaq25@gmail.com";
+ $subject = "Response from website or can be any";
+ $headers = " from :" $email; 
+ $txt = "you have received an email from  .$username".
+    "\n\n"."Email of the sender is" .$email. "\n\n". 
+ "Contact of sender is :" .$number ."\n\n". "message is :".$message; 
+ $email($mailto, $subject, $txt, $headers); 
+ header("location:index.html"); 
+ }
+?> 
